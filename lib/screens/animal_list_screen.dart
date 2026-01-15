d379a0b4bc19d4282d71b4b0dac15938bb91f0a3
Jessy123123 <xinyuanpeng1@gmail.com>
2026-01-15 10:19:36 +0800

    --

--- lib/screens/animal_list_screen.dart
+++ lib/screens/animal_list_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import '../services/cctv_simulator.dart';
+import '../../cctv_simulator.dart';
 
 class AnimalListScreen extends StatefulWidget {
   const AnimalListScreen({super.key});
